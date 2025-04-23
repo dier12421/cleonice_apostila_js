0f1191ff643fd9d9ebb127661bf39a5d432f26b6
dier <161588493+dier12421@users.noreply.github.com>
2025-04-23 10:42:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+ var nomeDaVariavel = "valor da variável";
+ var outraVariavel = 'valor da outra variável';
+ var variavelIdade = 22;
+ var variavelTenhoCabeloBranco = false;
+ var variavelIndefinida ;
+ var variavelTexto = "texto";
+ var variavelNumero1 = 23;
+ var variavelNumero2 = 11
+ console.log("That's All Folks");
+ console.log(nomeDaVariavel);
+console.log(outraVariavel);
+console.log(variavelIdade);
+console.log(variavelTenhoCabeloBranco);
+console.log(variavelIndefinida);
+console.log(variavelTexto);
+console.log(variavelNumero1 + variavelNumero2); // Soma dos números
