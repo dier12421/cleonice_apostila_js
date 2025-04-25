97ef7d944e1bc71a15f55ae82142e65e52521b6b
dier <161588493+dier12421@users.noreply.github.com>
2025-04-25 12:12:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
+let expressao = '';
 
- var nomeDaVariavel = "valor da variável";
- var outraVariavel = 'valor da outra variável';
- var variavelIdade = 22;
- var variavelTenhoCabeloBranco = false;
- var variavelIndefinida ;
- var variavelTexto = "texto";
- var variavelNumero1 = 23;
- var variavelNumero2 = 11
- console.log("That's All Folks");
- console.log(nomeDaVariavel);
-console.log(outraVariavel);
-console.log(variavelIdade);
-console.log(variavelTenhoCabeloBranco);
-console.log(variavelIndefinida);
-console.log(variavelTexto);
-console.log(variavelNumero1 + variavelNumero2); // Soma dos números
+function adicionarNumero(numero) {
+  expressao += numero;
+  atualizarResultado();
+}
+
+function operacao(operador) {
+  expressao += operador;
+  atualizarResultado();
+}
+
+function atualizarResultado() {
+  document.getElementById('resultado').value = expressao;
+}
+
+function calcular() {
+  try {
+    const resultado = eval(expressao); // Avalia a expressão
+    expressao = resultado.toString();
+    atualizarResultado();
+  } catch (error) {
+    expressao = 'Erro';
+    atualizarResultado();
+  }
+}
